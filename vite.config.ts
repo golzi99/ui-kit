f30f07bd52598bf0e19abbeb9071727d6893cad0
golzi99 <golzi99@gmail.com>
2025-09-27 20:34:47 +0300

    update jsons

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { resolve } from 'node:path'
+import { dependencies, devDependencies } from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'iu-kit-bundler',
+      // the proper extensions will be added
+      fileName: 'index',
+      formats: ['es']
+    },
+    rollupOptions: {
+      external: ['react/jsx-runtime',...Object.keys(dependencies), ...Object.keys(devDependencies)],
+    },
+    target: 'esnext',
+    sourcemap: true
+  },
 })
